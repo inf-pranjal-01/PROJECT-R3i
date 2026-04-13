ef620cc993767ae034bd13d55d743c3f1fe8efb3
PRANJAL TIWARI <pranjal.tiwari2486@gmail.com>
2026-04-13 07:50:28 +0530

    render ready

--- firebase_client.py
+++ firebase_client.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 
-cred = credentials.Certificate('project-r3i-firebase-adminsdk.json')
+cred = credentials.Certificate('/etc/secrets/project-r3i-firebase-adminsdk.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
